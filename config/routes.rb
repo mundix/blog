f8ac0397a1fb64a834e3362f7a724b37e0e27405
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-01-27 18:57:00 -0400

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  # resources :posts -> Esto va cambiar como ahora los post tienen comment, este utilziar otro recurso commets
+  resources :posts do
+    resources :comments
+  end
   root "posts#index"
 end
